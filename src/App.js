fd7433185b2a4dfe5d4b50b373253150caac528a
Martin Stamm <martin.stamm@student.hpi.de>
2018-10-10 15:11:17 +0200

    Load Data from API

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const axios = require('axios');
+let request = require('request-promise');
+
+const API_URL = "https://fjgqer7ard.execute-api.eu-central-1.amazonaws.com/default/easywash"
+const API_ARG = "roomId"
+
+
+
 
 class RoomForm extends Component
 {
@@ -35,30 +43,103 @@ class RoomForm extends Component
   }
 }
 
+
 class Room extends Component {
   constructor(props){
     super(props)
-    this.state = {}
+    //Phases: 0 - Input of ROOM
+    //        1 - Fetching data
+    //        2 - finished
+    // Could probably be done nicer, we just have to deal with this for now
+    this.state = {roomData: null}
+  }
+
+  componentDidMount() {
+    this._asyncRequest = this.getRoomData().then(
+      externalData => {
+        this._asyncRequest = null;
+        this.setState({roomData: externalData});
+      }
+    );
+  }
+
+  componentWillUnmount() {
+    if (this._asyncRequest) {
+      this._asyncRequest.cancel();
+    }
+  }
+
+  async getRoomData(){
+    console.log(API_URL+'?'+API_ARG+'='+this.props.roomNr)
+    var config = {
+      headers: {'Access-Control-Allow-Origin': '*'}
+    };
+    return axios.get(API_URL+'?'+API_ARG+'='+this.props.roomNr, config)
+      .then((body) =>{
+        return body;
+      })
+      .catch((err)=>{
+        console.error(err)
+        throw(err)
+      }
+    )
+  }
+
+
+
+  render() {
+    console.log("rendering Room")
+    console.log(this.state.roomData)
+    if(this.state.roomData === null)
+    {
+      console.log("Loading")
+      return (<div> Loading... </div>)
+    }
+    else{
+      return (<div>{JSON.stringify(this.state.roomData)}</div>)
+    }
+  }
+
+}
+
+
+class Container extends Component {
+
+  constructor(props){
+    super(props)
+    //Phases: 0 - Input of ROOM
+    //        1 - Fetching data
+    //        2 - finished
+    // Could probably be done nicer, we just have to deal with this for now
+    this.state = {phase: 0}
   }
 
   //Arrow Function so 'this' is bound
   handleRoomInput = (value) => {
     console.log('Room Component value:' + value)
     this.setState({roomNr: value})
-    console.log(this.state)
+  }
+
+  renderRoomInfo(){
+    if (this.state.roomNr)
+    {
+      console.log(this.state.roomNr)
+      return <Room roomNr={this.state.roomNr} />
+    }
   }
 
   render() {
-    if (!this.state.roomNr)
-    {
-      return <RoomForm onRoomInput={this.handleRoomInput}/>
+      return (
+        <div>
+          <RoomForm onRoomInput={this.handleRoomInput}/>
+          {this.renderRoomInfo()}
+        </div>
+      )
     }
-    else
-    {
-      return <div />
-    }
-  }
+
 }
 
 
-export default Room;
+
+
+export default Container;

9d8796d38f4a46dcec090f37d35f042e28a81faf
Martin Stamm <martin.stamm@student.hpi.de>
2018-10-12 12:58:31 +0200

    Add Stats for Nerds

--- src/machienes.js
+++ src/machienes.js
@@ -1,29 +1,41 @@
 import React from 'react'
+import {Popup} from 'semantic-ui-react'
 
-function Machine(props){
-  if(props.status['waschgang'] == 0)
-  {
-    //Machine not in use
-    return(
-      <div class="ui message green">
-      <h2 class="ui header">{props.title} {props.status['mnr']}</h2>
-            Frei
-      </div>
-          )
-  }
-  else
-  {
-    return(
-      <div class="ui message red">
-      <h2 class="ui header">{props.title} {props.status['mnr']}</h2>
-        Restzeit: {props.status['restzeit']} Minuten <br />
-        Programm: {props.status['solltemperatur']}°
-      </div>
-          )
+class Machine extends React.Component
+{
+  render(){
+    if(this.props.status['waschgang'] == 0)
+    {
+      //Machine not in use
+      return(
+        <Popup trigger={
+            <div class="ui message green">
+                <h2 class="ui header">{this.props.title} {this.props.status['mnr']}</h2>
+                      Frei
+              </div>
+        }
+        content={<div style={{whiteSpace: 'pre', textAlign: 'left'}}>{JSON.stringify(this.props.status, null, 2)}</div>}
+        on='hover'
+        mouseEnterDelay={1000}/>
+            )
 
+    }
+    else
+    {
+      return(
+        <Popup trigger={  <div class="ui message red">
+            <h2 class="ui header">{this.props.title} {this.props.status['mnr']}</h2>
+              Restzeit: {this.props.status['restzeit']} Minuten <br />
+              Programm: {this.props.status['solltemperatur']}°
+        </div>}
+        content={<div style={{whiteSpace: 'pre', textAlign: 'left'}}>{JSON.stringify(this.props.status, null, 2)}</div>}
+        on='hover'
+        mouseEnterDelay={1000}/>
+            )
+
+    }
   }
 }
-
 
 function Washer(props){
   return <Machine status={props.status} title="Waschmaschine" />
